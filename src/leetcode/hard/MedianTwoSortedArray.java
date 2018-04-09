7b8080b66a0a83c50094a8ea055b6cdead24a8d7
Saksham Gangwar <sakshamgangwar19@gmail.com>
2018-04-08 21:24:42 -0700

    new file

---
+++ src/leetcode/hard/MedianTwoSortedArray.java
@@ -0,0 +1,21 @@
+package leetcode.hard;
+
+public class MedianTwoSortedArray {
+	public static void main(String args[]){
+		Solution1 solution = new Solution1();
+		int[] num1 = new int[]{1,3,8,9,15};
+		int[] num2 = new int[]{7,11,18,19,21,25};
+		System.out.println(solution.findMedianSortedArrays(num1,num2));
+	
+		
+		
+	}
+}
+
+class Solution1 {
+public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+	return 0;
+    }
+	
+}
